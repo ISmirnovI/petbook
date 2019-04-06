07ec564625dab8ec0a1e6eeca73352e583e8e513
|Smirnov| <krugvik@mail.ru>
2019-04-06 18:04:32 +0300

    feat: add Pet and fix compile errors

---
+++ src/main/java/com/smirnov/petbook/Pet.java
@@ -0,0 +1,59 @@
+package com.smirnov.petbook;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer petId;
+    private String petName;
+    private Integer petAge;
+    private String petGender;
+    private String petType;
+
+
+    public String getPetGender() {
+        return petGender;
+    }
+
+    public void setPetGender(String petGender) {
+        this.petGender = petGender;
+    }
+
+
+    public Integer getId() {
+        return petId;
+    }
+
+    public void setId(Integer id) {
+        this.petId = id;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public Integer getPetAge() {
+        return petAge;
+    }
+
+    public void setPetAge(Integer petAge) {
+        this.petAge = petAge;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+}
